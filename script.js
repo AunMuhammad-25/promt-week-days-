3d439c728575f1919205cb810655ee023fb73fee
qamberali <qamberali4904@gmail.com>
2024-11-10 16:27:10 +0500

    prompt(week days)

---
+++ script.js
@@ -0,0 +1,29 @@
+let num=prompt("enter a number from 1 to 7")
+if(num==1){
+    alert("monday")
+}else if(num==2){
+    alert("tuesday")
+
+}
+else if(num==3){
+    alert("wednesday")
+
+}
+else if(num==4){
+    alert("thursday")
+
+}
+else if(num==5){
+    alert("friday")
+
+}
+else if(num==6){
+    alert("saturday")
+
+}else if(num==7){
+    alert("sunday")
+
+}else{
+    alert("invalid number")
+
+}
